823ef01f20e0c6f69793bc49b7c7b30a1f56e464
高新成 <gaoxincheng@yolanda.hk>
2023-11-13 20:30:49 +0800

    更新底部

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -13,6 +13,10 @@ export default defineConfig({
     siteTitle: "GXC の 笔记",
     logo: "/logo.webp",
     lastUpdated: true,
+    footer: {
+      message: 'Released under the <a href="https://github.com/xincheng-1999/vitepress-doc">MIT License</a>.',
+      copyright: 'Copyright © 2019-present <a href="https://github.com/xincheng-1999/vitepress-doc">GXC</a>',
+    },
     // theme-level options
     sidebar: {
       "/front-end/interview-questions/": [
