6d9beb03f514cb33992b48d71666a72e2a683c82
gaoxincheng <gaoxincheng@bylcloud.com>
2024-08-16 15:58:29 +0800

    update: 添加看板娘

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,3 +1,18 @@
 import DefaultTheme from "vitepress/theme";
 import "./custom.css";
-export default DefaultTheme;
+
+export default {
+  ...DefaultTheme,
+  async enhanceApp() {
+    if (!import.meta.env.SSR) {
+      const { loadOml2d } = await import('oh-my-live2d');
+      loadOml2d({
+        models: [
+          {
+            path: 'https://model.oml2d.com/HK416-1-normal/model.json'
+          }
+        ]
+      });
+    }
+  }
+};
