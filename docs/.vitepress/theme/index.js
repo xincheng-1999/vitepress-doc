90ac67d66b4a575e6f583deb4fa203bec136803a
project <1349955700@qq.com>
2025-09-01 03:09:28 +0000

    添加live2d模型

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,6 +1,15 @@
 import DefaultTheme from "vitepress/theme";
 import "./custom.css";
 
+const modelPaths = [
+  new URL('./models/HK416-1-normal/model.json', import.meta.url).href,
+  new URL('./models/HK416-2-destroy/model.json', import.meta.url).href,
+  new URL('./models/HK416-2-normal/model.json', import.meta.url).href,
+  new URL('./models/Kar98k-normal/model.json', import.meta.url).href,
+  new URL('./models/kp31/model.json', import.meta.url).href,
+  new URL('./models/rem/model.json', import.meta.url).href,
+];
+
 export default {
   ...DefaultTheme,
   async enhanceApp() {
@@ -9,19 +18,8 @@ export default {
       loadOml2d({
         models: [
           {
-            path: [
-              'https://unpkg.com/live2d-widget-model-shizuku@latest/assets/shizuku.model.json',
-              'https://unpkg.com/live2d-widget-model-shizuku@latest/assets/shizuku.physics.json',
-              'https://unpkg.com/live2d-widget-model-shizuku@latest/assets/shizuku.pose.json',
-              'https://unpkg.com/live2d-widget-model-miku@latest/assets/miku.model.json',
-              'https://model.oml2d.com/senko_normals/senko.model3.json',
-              'https://cdn.jsdelivr.net/gh/Eikanya/Live2d-model/Live2D/Senko_Normals/senko.model3.json',
-              'https://model.oml2d.com/HK416-2-normal/model.json',
-              'https://model.oml2d.com/HK416-2-destroy/model.json',
-              'https://model.oml2d.com/HK416-2-normal/model.json',
-              'https://model.oml2d.com/Kar98k-normal/model.json',
-            ],
-            scale: 0.08
+            path: modelPaths,
+            scale: 0.06,
           }
         ],
         dockedPosition: 'right'
